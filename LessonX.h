8cbeeea4638f2986735c70f3e7a8e46a43a7e362
IloveIcarus <qq904870644@163.com>
2017-06-30 08:19:20 +0800

    person
    17 6 30
    simple design
    yuhua zhao

---
+++ LessonX.h
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+/////////////////////////////////////////////////////////////////////////////////
+#ifndef _LESSON_X_H_
+#define _LESSON_X_H_
+//
+#include <Windows.h>
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+// 游戏总管类。负责处理游戏主循环、游戏初始化、结束等工作
+class	CGameMain
+{
+private:
+	int				m_iGameState;				// 游戏状态，0：结束或者等待开始；1：初始化；2：游戏进行中
+	CSprite *p1;
+	CSprite *p2;
+	float fl,fr,fu;
+public:
+	CGameMain();            //构造函数
+	~CGameMain();           //析构函数  
+
+	// Get方法
+	int				GetGameState()											{ return m_iGameState; }
+	
+	// Set方法
+	void			SetGameState( const int iState )				{ m_iGameState	=	iState; }
+	
+	// 游戏主循环等
+	void			GameMainLoop( float	fDeltaTime );
+	void			GameInit();
+	void			GameRun( float fDeltaTime );
+	void			GameEnd();
+	void			OnKeyDown( const int iKey, const bool bAltPress, const bool bShiftPress, const bool bCtrlPress );
+	void			OnKeyUp( const int iKey );
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+// 
+extern CGameMain	g_GameMain;
+
+#endif // _LESSON_X_H_
